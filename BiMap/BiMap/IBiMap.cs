8841fbe3ce826e68932cef99b728cd859afa8bf3
TahaZahit <tahazahit@gmail.com>
2021-08-26 19:41:47 +0300

    Update IBiMap.cs

--- BiMap/BiMap/IBiMap.cs
+++ BiMap/BiMap/IBiMap.cs
@@ -1,1 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
+namespace BiMap
+{
+	public interface IBiMap<TLeft, TRight>
+	{
+		IEnumerable<KeyValuePair<TLeft, TRight>> EnumerateLeftToRight();
+		IEnumerable<KeyValuePair<TRight, TLeft>> EnumerateRightToLeft();
+
+		TRight GetLeftToRight(TLeft key);
+		bool TryGetLeftToRight(TLeft key, out TRight value);
+		TLeft GetRightToLeft(TRight key);
+		bool TryGetRightToLeft(TRight key, out TLeft value);
+
+		int Count { get; }
+	}
+}
