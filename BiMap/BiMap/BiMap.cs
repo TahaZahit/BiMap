398e5e00ba98520533dafcbca925c378048165e3
Taha Zahit Hakyemez <taha.zahit@kivierp.com.tr>
2026-01-28 17:07:46 +0300

    Refactor BiMap to .NET Standard 2.0 with thread safety and cleanup gitignore

--- BiMap/BiMap/BiMap.cs
+++ BiMap/BiMap/BiMap.cs
@@ -1,80 +1,292 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Threading;
 
 namespace BiMap
 {
-	public class BiMap<TLeft, TRight> : IBiMap<TLeft, TRight>
-	{
-		private Dictionary<TLeft, TRight> LeftToRight;
-		private Dictionary<TRight, TLeft> RightToLeft;
+    /// <summary>
+    /// A thread-safe, bidirectional map allowing mapping between two unique keys.
+    /// Implementation targets .NET Standard 2.0.
+    /// </summary>
+    /// <typeparam name="TLeft">The type of the left key.</typeparam>
+    /// <typeparam name="TRight">The type of the right key.</typeparam>
+    public class BiMap<TLeft, TRight> : IBiMap<TLeft, TRight>
+    {
+        private readonly Dictionary<TLeft, TRight> _leftToRight;
+        private readonly Dictionary<TRight, TLeft> _rightToLeft;
+        private readonly ReaderWriterLockSlim _lock;
+        private bool _disposed;
 
-		public int Count
-		{
-			get
-			{
-				return this.LeftToRight.Count;
-			}
-		}
+        /// <summary>
+        /// Gets the number of elements in the BiMap.
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                _lock.EnterReadLock();
+                try
+                {
+                    return _leftToRight.Count;
+                }
+                finally
+                {
+                    _lock.ExitReadLock();
+                }
+            }
+        }
 
-		public BiMap(IEqualityComparer<TLeft> leftComp = null, IEqualityComparer<TRight> rightComp = null)
-		{
-			this.LeftToRight = new Dictionary<TLeft, TRight>(leftComp);
-			this.RightToLeft = new Dictionary<TRight, TLeft>(rightComp);
-		}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BiMap{TLeft, TRight}"/> class.
+        /// </summary>
+        /// <param name="leftComp">Equality comparer for the left key.</param>
+        /// <param name="rightComp">Equality comparer for the right key.</param>
+        public BiMap(IEqualityComparer<TLeft> leftComp = null, IEqualityComparer<TRight> rightComp = null)
+        {
+            _leftToRight = new Dictionary<TLeft, TRight>(leftComp);
+            _rightToLeft = new Dictionary<TRight, TLeft>(rightComp);
+            _lock = new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
+        }
 
-		public bool AddPair(TLeft left, TRight right)
-		{
-			if (LeftToRight.ContainsKey(left) || RightToLeft.ContainsKey(right))
-			{
-				return false;
-			}
+        /// <summary>
+        /// Attempts to add a pair to the map. Returns false if either key already exists.
+        /// </summary>
+        public bool Add(TLeft left, TRight right)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                if (_leftToRight.ContainsKey(left) || _rightToLeft.ContainsKey(right))
+                {
+                    return false;
+                }
 
-			LeftToRight.Add(left, right);
-			RightToLeft.Add(right, left);
+                _leftToRight.Add(left, right);
+                _rightToLeft.Add(right, left);
+                return true;
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+        
+        // Backward compatibility alias likely expected by existing code if reusing class.
+        public bool AddPair(TLeft left, TRight right) => Add(left, right);
 
-			return true;
-		}
+        /// <summary>
+        /// Removes the mapping associated with the specified left key.
+        /// </summary>
+        public bool RemoveByLeft(TLeft left)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                if (_leftToRight.TryGetValue(left, out TRight right))
+                {
+                    _leftToRight.Remove(left);
+                    _rightToLeft.Remove(right);
+                    return true;
+                }
+                return false;
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
 
-		public IEnumerable<KeyValuePair<TLeft, TRight>> EnumerateLeftToRight()
-		{
-			return LeftToRight;
-		}
+        /// <summary>
+        /// Removes the mapping associated with the specified right key.
+        /// </summary>
+        public bool RemoveByRight(TRight right)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                if (_rightToLeft.TryGetValue(right, out TLeft left))
+                {
+                    _rightToLeft.Remove(right);
+                    _leftToRight.Remove(left);
+                    return true;
+                }
+                return false;
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
 
-		public IEnumerable<KeyValuePair<TRight, TLeft>> EnumerateRightToLeft()
-		{
-			return RightToLeft;
-		}
+        /// <summary>
+        /// Clears all mappings.
+        /// </summary>
+        public void Clear()
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                _leftToRight.Clear();
+                _rightToLeft.Clear();
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
 
-		public TRight GetLeftToRight(TLeft key)
-		{
-			TRight result;
+        /// <summary>
+        /// Determines whether the map contains the specified left key.
+        /// </summary>
+        public bool ContainsLeft(TLeft left)
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                return _leftToRight.ContainsKey(left);
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
 
-			if (!this.TryGetLeftToRight(key, out result))
-				throw new KeyNotFoundException("Key: " + key.ToString() + " not found in left-to-right collection");
+        /// <summary>
+        /// Determines whether the map contains the specified right key.
+        /// </summary>
+        public bool ContainsRight(TRight right)
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                return _rightToLeft.ContainsKey(right);
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
 
-			return result;
-		}
+        /// <summary>
+        /// Gets the right value associated with the specified left key. Throws KeyNotFoundException if not found.
+        /// </summary>
+        public TRight GetLeftToRight(TLeft key)
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                if (!_leftToRight.TryGetValue(key, out TRight result))
+                    throw new KeyNotFoundException($"Key: {key} not found");
+                return result;
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
 
-		public bool TryGetLeftToRight(TLeft key, out TRight value)
-		{
-			return this.LeftToRight.TryGetValue(key, out value);
-		}
+        /// <summary>
+        /// Tries to get the right value associated with the specified left key.
+        /// </summary>
+        public bool TryGetLeftToRight(TLeft key, out TRight value)
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                return _leftToRight.TryGetValue(key, out value);
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
 
-		public TLeft GetRightToLeft(TRight key)
-		{
-			TLeft result;
+        /// <summary>
+        /// Gets the left value associated with the specified right key. Throws KeyNotFoundException if not found.
+        /// </summary>
+        public TLeft GetRightToLeft(TRight key)
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                if (!_rightToLeft.TryGetValue(key, out TLeft result))
+                    throw new KeyNotFoundException($"Key: {key} not found");
+                return result;
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
 
-			if (!this.TryGetRightToLeft(key, out result))
-				throw new KeyNotFoundException("Key: " + key.ToString() + " not found in right-to-left collection");
+        /// <summary>
+        /// Tries to get the left value associated with the specified right key.
+        /// </summary>
+        public bool TryGetRightToLeft(TRight key, out TLeft value)
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                return _rightToLeft.TryGetValue(key, out value);
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
 
-			return result;
-		}
+        /// <summary>
+        /// Returns a thread-safe snapshot of the Left-to-Right mappings.
+        /// </summary>
+        public IEnumerable<KeyValuePair<TLeft, TRight>> EnumerateLeftToRight()
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                return new List<KeyValuePair<TLeft, TRight>>(_leftToRight);
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
 
-		public bool TryGetRightToLeft(TRight key, out TLeft value)
-		{
-			return this.RightToLeft.TryGetValue(key, out value);
-		}
-	}
+        /// <summary>
+        /// Returns a thread-safe snapshot of the Right-to-Left mappings.
+        /// </summary>
+        public IEnumerable<KeyValuePair<TRight, TLeft>> EnumerateRightToLeft()
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                return new List<KeyValuePair<TRight, TLeft>>(_rightToLeft);
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
+
+        /// <summary>
+        /// Disposes the ReaderWriterLockSlim.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _lock?.Dispose();
+                }
+                _disposed = true;
+            }
+        }
+    }
 }
--- BiMap/BiMap/IBiMap.cs
+++ BiMap/BiMap/IBiMap.cs
@@ -1,20 +1,78 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace BiMap
 {
-	public interface IBiMap<TLeft, TRight>
-	{
-		IEnumerable<KeyValuePair<TLeft, TRight>> EnumerateLeftToRight();
-		IEnumerable<KeyValuePair<TRight, TLeft>> EnumerateRightToLeft();
+    /// <summary>
+    /// Represents a thread-safe generic bidirectional map.
+    /// </summary>
+    /// <typeparam name="TLeft">The type of the left key.</typeparam>
+    /// <typeparam name="TRight">The type of the right key.</typeparam>
+    public interface IBiMap<TLeft, TRight> : IDisposable
+    {
+        /// <summary>
+        /// Gets the number of pairs in the map.
+        /// </summary>
+        int Count { get; }
 
-		TRight GetLeftToRight(TLeft key);
-		bool TryGetLeftToRight(TLeft key, out TRight value);
-		TLeft GetRightToLeft(TRight key);
-		bool TryGetRightToLeft(TRight key, out TLeft value);
+        /// <summary>
+        /// Adds a pair to the map. Returns false if either key already exists.
+        /// </summary>
+        bool Add(TLeft left, TRight right);
 
-		int Count { get; }
-	}
+        /// <summary>
+        /// Removes a pair by the left key.
+        /// </summary>
+        bool RemoveByLeft(TLeft left);
+
+        /// <summary>
+        /// Removes a pair by the right key.
+        /// </summary>
+        bool RemoveByRight(TRight right);
+
+        /// <summary>
+        /// Clears the map.
+        /// </summary>
+        void Clear();
+
+        /// <summary>
+        /// Checks if the left key exists.
+        /// </summary>
+        bool ContainsLeft(TLeft left);
+
+        /// <summary>
+        /// Checks if the right key exists.
+        /// </summary>
+        bool ContainsRight(TRight right);
+
+        /// <summary>
+        /// Gets the right value associated with the left key.
+        /// </summary>
+        TRight GetLeftToRight(TLeft left);
+
+        /// <summary>
+        /// Tries to get the right value associated with the left key.
+        /// </summary>
+        bool TryGetLeftToRight(TLeft left, out TRight right);
+
+        /// <summary>
+        /// Gets the left value associated with the right key.
+        /// </summary>
+        TLeft GetRightToLeft(TRight right);
+
+        /// <summary>
+        /// Tries to get the left value associated with the right key.
+        /// </summary>
+        bool TryGetRightToLeft(TRight right, out TLeft left);
+
+        /// <summary>
+        /// Returns a thread-safe snapshot of the Left-to-Right mappings.
+        /// </summary>
+        IEnumerable<KeyValuePair<TLeft, TRight>> EnumerateLeftToRight();
+
+        /// <summary>
+        /// Returns a thread-safe snapshot of the Right-to-Left mappings.
+        /// </summary>
+        IEnumerable<KeyValuePair<TRight, TLeft>> EnumerateRightToLeft();
+    }
 }
